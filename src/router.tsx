bc63dd662839b253b0b517d5ff6f806c047a9416
Miguel Cobian <miguelcobian99@gmail.com>
2024-04-24 17:41:35 -0600

    Feat. Implement router with React Router Dom

---
+++ src/router.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Layout } from "./layouts/Layout";
+import { Products } from "./views/Products";
+import { NewProduct } from "./views/NewProduct";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            {
+                element: <Products />,
+                index: true,
+            },
+            {
+                path: 'productos/nuevo',
+                element: <NewProduct />
+            }
+        ]
+    }
+]);
