78a62cb08894efdc7b4a015855776ad1706c3b9a
Miguel Cobian <miguelcobian99@gmail.com>
2024-04-24 20:00:05 -0600

    Chore. Validate and send petition to the REST API

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,8 +1,28 @@
+import { safeParse } from "valibot";
+import { DraftProductSchema } from "../types";
+import axios from "axios";
 
 type ProductData = {
     [k: string]: FormDataEntryValue;
 }
 
 export async function addProduct(data: ProductData) {
-    console.log(data);
+    try {
+        const result = safeParse(DraftProductSchema, {
+            name: data.name,
+            price: +data.price,
+        });
+
+        if (result.success) {
+            const url = `${import.meta.env.VITE_API_URL}/api/products`;
+            const { data } = await axios.post(url, {
+                name: result.output.name,
+                price: result.output.price,
+            })
+        } else {
+            throw new Error('Datos no válidos');
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
