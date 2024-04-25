30bc24502d11a3a97745fa2577c1d1af4912069f
Miguel Cobian <miguelcobian99@gmail.com>
2024-04-24 18:20:48 -0600

    Chore. Define form action

--- src/views/NewProduct.tsx
+++ src/views/NewProduct.tsx
@@ -1,4 +1,10 @@
-import { Link } from "react-router-dom"
+import { Link, Form } from "react-router-dom";
+
+export async function action() {
+    console.log('Desde ACTION');
+
+    return {};
+}
 
 export const NewProduct = () => {
     return (
@@ -12,6 +18,45 @@ export const NewProduct = () => {
                     Volver a productos
                 </Link>
             </div>
+
+            <Form
+                className="mt-10"
+                method="POST"
+                action=''
+            >
+
+                <div className="mb-4">
+                    <label
+                        className="text-gray-800"
+                        htmlFor="name"
+                    >Nombre producto:</label>
+                    <input
+                        id="name"
+                        type="text"
+                        className="mt-2 block w-full p-3 bg-gray-50"
+                        placeholder="Nombre del producto"
+                        name="name"
+                    />
+                </div>
+                <div className="mb-4">
+                    <label
+                        className="text-gray-800"
+                        htmlFor="price"
+                    >Precio:</label>
+                    <input
+                        id="price"
+                        type="number"
+                        className="mt-2 block w-full p-3 bg-gray-50"
+                        placeholder="Precio producto. ej. 200, 300"
+                        name="price"
+                    />
+                </div>
+                <input
+                    type="submit"
+                    className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
+                    value="Registrar producto"
+                />
+            </Form>
         </>
     )
 }
