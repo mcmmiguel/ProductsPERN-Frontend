c27be025f8d7a9e7fc7a6e614dbb0a77aa2b56be
Miguel Cobian <miguelcobian99@gmail.com>
2024-04-24 19:34:49 -0600

    Feat. Get input form data with React Router Form

--- src/views/NewProduct.tsx
+++ src/views/NewProduct.tsx
@@ -1,12 +1,25 @@
-import { Link, Form } from "react-router-dom";
+import { Link, Form, useActionData } from "react-router-dom";
 
-export async function action() {
-    console.log('Desde ACTION');
+export async function action({ request }) {
+    const data = Object.fromEntries(await request.formData());
+
+    let error = '';
+
+    if (Object.values(data).includes('')) {
+        error = 'Todos los campos son obligatorios';
+    }
+
+    if (error.length) {
+        return error;
+    }
 
     return {};
 }
 
 export const NewProduct = () => {
+
+    const error = useActionData();
+
     return (
         <>
             <div className="flex justify-between">
