d2e92c0513ca1194def80102064c7af066d45bd5
Miguel Cobian <miguelcobian99@gmail.com>
2024-04-29 17:44:50 -0600

    Feat. Redirect to the id product url

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { Product } from "../types"
 import { formatCurrency } from "../utils";
 
@@ -8,6 +9,7 @@ type ProductDetailProps = {
 export const ProductDetails = ({ product }: ProductDetailProps) => {
 
     const isAvailable = product.availability;
+    const navigate = useNavigate();
 
     return (
         <tr className="border-b ">
@@ -22,7 +24,12 @@ export const ProductDetails = ({ product }: ProductDetailProps) => {
             </td>
             <td className="p-3 text-lg text-gray-800 ">
                 <div className="flex gap-2 items-center">
-                    <button>Editar</button>
+                    <button
+                        onClick={() => navigate(`/productos/${product.id}/editar`)}
+                        className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
+                    >
+                        Editar
+                    </button>
                 </div>
             </td>
         </tr>
